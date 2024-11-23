6fb252beb67f40e540c6d284d89850fa0c3cdd2e
t-yokota <yokota.0529@gmail.com>
2024-11-23 18:22:22 +0900

    install.sh - Added function that creates symbolic links of oh-my-zsh theme files - Refactered scripts

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
 DOTPATH=~/dotfiles
-cd $DOTPATH
+OH_MY_ZSH_THEMES=~/.oh-my-zsh/themes
 
-for f in .??*
-do
+cd "$DOTPATH" || { echo "Error: Could not cd to $DOTPATH"; exit 1; }
+
+echo "Creating symbolic links for .dotfiles in $DOTPATH"
+for f in .??*; do
     [ "$f" = ".git" ] && continue
-    ln -snfv $DOTPATH/$f $HOME/$f
+    ln -snfv "$DOTPATH/$f" "$HOME/$f"
 done
+
+if [ -d "$OH_MY_ZSH_THEMES" ]; then
+    echo "Creating symbolic links for .zsh-theme files in $OH_MY_ZSH_THEMES"
+    for theme in "$DOTPATH"/*.zsh-theme; do
+        ln -snfv "$theme" "$OH_MY_ZSH_THEMES/$(basename "$theme")"
+    done
+else
+    echo "Directory $OH_MY_ZSH_THEMES does not exist. Skipping theme linking."
+fi
